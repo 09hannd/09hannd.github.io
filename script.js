9f4aac6a046b5fad5be109f0e8b8c2ec0ddbabac
09hannd <09hannd@oxtedschool.co.uk>
2016-02-28 21:53:11 +0000

    Alpha 0.0.1.6
    Added code to prevent duplicate card plays.

--- script.js
+++ script.js
@@ -3,27 +3,36 @@ String.prototype.replaceAll = function(search, replacement) {
     return target.replace(new RegExp(search, 'g'), replacement);
 };
 
-function randInt(min,max)
-{
+function randInt(min,max){
     return Math.floor(Math.random()*(max-min+1)+min);
 }
 
 
+var cards = [];
+var playedQuestions = []
+
+
 function pickQuestion(){
     var isQuestion = false;
+    var hasPlayedQuestion = false;
     var card, cardType, cardText, cardId;
     while(!isQuestion){
         var number = randInt(0,database.length);
         card = database[number];
         cardType = card.cardType;
+        while(playedQuestions.indexOf(card) > -1){
+            var number = randInt(0,database.length);
+            card = database[number];
+            cardType = card.cardType;
+        }
         if(cardType == "Q")isQuestion = true;
     }
     
     cardText = card.text.replaceAll("_", "<i>blank</i>");
     cardId = card.id;
+    playedQuestions.push(card);
     var newElm = "<div id='currentQuestion' class='w3-card-4 w3-black w3-margin card' style='width:80%;max-width:350px;'><div class='w3-container w3-center'><br><h5>"+cardText+"</h5></div><button class='w3-btn-block w3-green' onclick='pickQuestion();'>Swap <i class='fa fa-refresh'></i></button></div>";
     document.getElementById("qu").innerHTML = newElm;
-    document.getElementById("currentQuestion").style.left = window.innerWidth - document.getElementById("currentQuestion").outerWidth / 2;
 }
 
 pickQuestion();
