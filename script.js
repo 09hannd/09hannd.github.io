ab0fe4d31f1019f2b007a512ae502b3ecf3f9e90
09hannd <09hannd@oxtedschool.co.uk>
2016-02-28 19:35:16 +0000

    New script

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+String.prototype.replaceAll = function(search, replacement) {
+    var target = this;
+    return target.replace(new RegExp(search, 'g'), replacement);
+};
+
+
+var answersEl = document.getElementById("answers");
+var questionsEl = document.getElementById("questions");
+
 for(x in database){
-    alert(database[x].text);
+    card = database[x];
+    cardType = card.cardType;
+    cardText = card.text.replaceAll("_", "<i>blank</i>");
+    cardId = card.id;
+    
+    if(cardType == "A"){
+        answersEl.innerHTML += cardText + "</br>";
+    }
+    if(cardType == "Q"){
+        questionsEl.innerHTML += cardText + "</br>";
+    }
 }
