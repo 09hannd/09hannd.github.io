5d5cfb66bedbb39a5d574841fd45c3dc907a4d6c
09hannd <09hannd@oxtedschool.co.uk>
2016-02-29 09:17:22 +0000

    Update writeDb.js

--- writeDb.js
+++ writeDb.js
@@ -1,3 +1,17 @@
 /*
 alpha code to write to database.
+This is gonna take some time!
 */
+
+$(document).ready(function(){
+    $("testButton").click(function(){
+        $.post("send.php",
+        {
+          name: "Donald Duck",
+          city: "Duckburg"
+        },
+        function(data,status){
+            alert("Data: " + data + "\nStatus: " + status);
+        });
+    });
+});
