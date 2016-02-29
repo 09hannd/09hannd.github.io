c6c193d8a3aec7c554b39fdd331b7deac884fe02
09hannd <09hannd@oxtedschool.co.uk>
2016-02-29 09:38:30 +0000

    Update writeDb.js

--- writeDb.js
+++ writeDb.js
@@ -5,13 +5,13 @@ This is gonna take some time!
 
 $(document).ready(function(){
     $("testButton").click(function(){
-        $.post("send.php",
+        $.post("/send.php",
         {
           name: "Donald Duck",
           city: "Duckburg"
         },
         function(data,status){
-            alert("Data: " + data + "\nStatus: " + status);
+            alert(data);
         });
     });
 });
