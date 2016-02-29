6fcfe5f78e87e0798c07db98eca48b3ca9a1cd2f
09hannd <09hannd@oxtedschool.co.uk>
2016-02-29 13:25:10 +0000

    Update script.js

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ function pickCards(){
     var cards = Array();
     for(var i = 0; i < 5; i++){
         var card = generateCard();
-        while(card.cardType == "Q"){
+        while(card.cardType == "A"){
             card = generateCard();
         }
         cards.push(card);
